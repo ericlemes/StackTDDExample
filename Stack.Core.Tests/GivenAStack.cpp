09af6870e78672ae7d0cca5eb48180ad7ea66c72
Eric Lemes <ericlemes@gmail.com>
2019-02-06 14:34:25 +0000

    Adds next red test: WhenPushingMultipleElementsShouldReturnExpectedValue

--- Stack.Core.Tests/GivenAStack.cpp
+++ Stack.Core.Tests/GivenAStack.cpp
@@ -11,3 +11,11 @@ TEST_F(GivenAStack, WhenPushingShouldAddElement) {
 	stack.Push(1);
 	ASSERT_EQ(1, stack.Pop());
 }
+
+TEST_F(GivenAStack, WhenPushingMultipleElementsShouldReturnExpectedValue) {
+	stack.Push(1);
+	stack.Push(2);
+
+	ASSERT_EQ(2, stack.Pop());
+	ASSERT_EQ(1, stack.Pop());
+}
